594fd87b077f0b3d5e649e647376eb318124e680
soudeepd <33814114+soudeepd@users.noreply.github.com>
2020-12-03 14:56:56 +0530

    moran's I calculation updated

--- Code/main_morani.R
+++ Code/main_morani.R
@@ -1,3 +1,5 @@
+
+# load relevant libraries
 library(readr)
 library(invgamma)
 library(goftest)
@@ -6,15 +8,44 @@ library(geosphere)
 library(ape)
 library(ggplot2)
 
-state_lat_long = unique(df[,c(2,3,4)])
+# run the main_data_prep script to get df
+View(df)
 
-weight_mat=as.matrix(distm(cbind(state_lat_long$long,state_lat_long$lat), fun = distHaversine))
-rho =0.001   #converting to Km
-weight_decay_mat=exp(-rho*weight_mat)
-diag(weight_decay_mat)=0
-moran_i_vec = vector()
-for (tmp in seq.Date(min(df$week),max(df$week), 7)) {
-  moran_i_vec = c(moran_i_vec, Moran.I(df %>% filter(week == (tmp)) %>% pull(log_prevalence),weight_decay_mat)$observed)
+# find the latitude longitude and prepare weight matrix
+state_lat_long = unique(df[,c("state","lat","long")])
+weight_mat = as.matrix(distm(cbind(state_lat_long$long,state_lat_long$lat), fun = distHaversine))
+
+# find exponential decay matrix
+rho = 0.001   # needed converting to Km
+weight_decay_mat = exp(-rho*weight_mat)
+diag(weight_decay_mat) = 0
+
+# find the Moran's I index for all weeks
+all_weeks = unique(df$week)
+moran_i_mat = matrix(nrow = length(all_weeks),ncol = 4)
+for (tmp in 1:length(all_weeks)){
+  moran_test = Moran.I(df %>% filter(week == all_weeks[tmp]) %>% pull(log_prevalence),weight_decay_mat)
+  moran_i_mat[tmp,] = unlist(moran_test)
 }
 
+# combine all results
+moran_result = data.frame(all_weeks,moran_i_mat)
+colnames(moran_result) = c("week","observed","expected","sd","pvalue")
+moran_result$significant = ifelse(moran_result$pvalue < 0.05,"significant","not significant")
 
+# see the plot
+moran_result %>%
+  ggplot(aes(x = week,y = observed)) +
+  geom_line() +
+  geom_point(aes(x = week,y = observed,pch = significant),size = 3) +
+  scale_shape_manual(values = c("significant" = 19,"not significant" = 1)) +
+  ylab("Moran's I statistic") +
+  theme_minimal() +
+  theme(legend.position = "bottom",
+        legend.text = element_text(size = 20),
+        legend.title = element_blank()) + 
+  ggsave(filename = "~/Documents/GitHub/COVID_Analysis/Code/morans_I.eps",
+         width = 300,
+         height = 200,
+         units = "mm",
+         dpi = 800)
