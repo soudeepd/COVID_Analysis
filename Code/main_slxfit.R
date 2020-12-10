92f1487632c52cb78407f713fa2f3a588b293dcd
soudeepd <33814114+soudeepd@users.noreply.github.com>
2020-12-10 08:49:03 +0530

    some modifications in SLX code

--- Code/main_slxfit.R
+++ Code/main_slxfit.R
@@ -7,16 +7,16 @@ library(sf)
 source("~/Documents/GitHub/COVID_Analysis/Code/main_dataprep.R")
 
 # take the training data and define it as a spatial object
-spdf <- training2
+spdf <- full_training2
 coordinates(spdf) <- ~ long + lat
 
 # create the neighbours list for the SLX code
 neib <- knn2nb(knearneigh(coordinates(spdf),longlat = TRUE))
-lw <- nb2listw(neib,style = "W") # here see documentation, you can use W/B/C/U/minimax/S
+lw <- nb2listw(neib,style = "B") # here see documentation, you can use W/B/C/U/minimax/S
 
 # define the regressors and then run the model
-regset = c("time","time_sq","prev_log_new_death","prev_log_prevalence","prev_log_prevalence_country")
-model_formula = paste("log_prevalence~",paste(regset,collapse = "+"))
+regset = c("time","time_sq","prev_log_new_death")
+model_formula = paste("log_prevalence~I(log(population))+",paste(regset,collapse = "+"))
 slxmodel <- lagsarlm(formula(model_formula),data = spdf,listw = lw)
 summary(slxmodel)
 
@@ -25,7 +25,7 @@ summary(slxmodel)
 preddf = df
 coordinates(preddf) <- ~ long + lat
 predneib <- knn2nb(knearneigh(coordinates(preddf),longlat = TRUE))
-predlw <- spdep::nb2listw(predneib,style = "W")
+predlw <- spdep::nb2listw(predneib,style = "B")
 
 slxpredict <- predict.sarlm(slxmodel,newdata = preddf,listw = predlw)
 df$slxfit = slxpredict
@@ -51,3 +51,8 @@ slxerror %>%
   ungroup() %>%
   mutate_if(is.numeric,round,digits = 3) %>%
   View()
+
+
+m2 = lm(formula(model_formula),data = spdf)
+summary(m2)
+df$slxfit = predict(m2,df)
